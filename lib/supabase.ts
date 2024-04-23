73826cf7ef61b2fb45ebc47da6b71effe4911d17
Arvid de Haas <arviddehaas@hotmail.nl>
2024-04-23 14:27:25 +0200

    added supabase types

---
+++ lib/supabase.ts
@@ -0,0 +1,299 @@
+// export type Tables<T extends keyof Database['public']['Tables']> = Database['public']['Tables'][T]['Row']
+// export type Enums<T extends keyof Database["public"]["Enums"]> = Database["public"]["Enums"][T];
+export type SupabaseChat = Database['public']['Tables']['chats']['Row']
+export type DocumentOwner =
+  Database['public']['Tables']['document_owners']['Row']
+export type DocumentSection =
+  Database['public']['Tables']['document_sections']['Row']
+export type SupabaseDocument = Database['public']['Tables']['documents']['Row']
+export type Tag = Database['public']['Tables']['tags']['Row']
+
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  public: {
+    Tables: {
+      chats: {
+        Row: {
+          created_at: string
+          id: string
+          payload: Json | null
+          user_id: string | null
+        }
+        Insert: {
+          created_at?: string
+          id: string
+          payload?: Json | null
+          user_id?: string | null
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          payload?: Json | null
+          user_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: 'chats_user_id_fkey'
+            columns: ['user_id']
+            isOneToOne: false
+            referencedRelation: 'users'
+            referencedColumns: ['id']
+          }
+        ]
+      }
+      document_owners: {
+        Row: {
+          document_id: number
+          id: number
+          owner_id: string
+        }
+        Insert: {
+          document_id: number
+          id?: never
+          owner_id?: string
+        }
+        Update: {
+          document_id?: number
+          id?: never
+          owner_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: 'public_document_owners_document_id_fkey'
+            columns: ['document_id']
+            isOneToOne: false
+            referencedRelation: 'documents'
+            referencedColumns: ['id']
+          },
+          {
+            foreignKeyName: 'public_document_owners_owner_id_fkey'
+            columns: ['owner_id']
+            isOneToOne: false
+            referencedRelation: 'users'
+            referencedColumns: ['id']
+          }
+        ]
+      }
+      document_sections: {
+        Row: {
+          content: string
+          document_id: number
+          embedding: string | null
+          id: number
+          metadata: Json | null
+        }
+        Insert: {
+          content: string
+          document_id: number
+          embedding?: string | null
+          id?: never
+          metadata?: Json | null
+        }
+        Update: {
+          content?: string
+          document_id?: number
+          embedding?: string | null
+          id?: never
+          metadata?: Json | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: 'public_document_sections_document_id_fkey'
+            columns: ['document_id']
+            isOneToOne: false
+            referencedRelation: 'documents'
+            referencedColumns: ['id']
+          }
+        ]
+      }
+      documents: {
+        Row: {
+          created_at: string
+          id: number
+          metadata: Json | null
+          name: string
+          source: string | null
+        }
+        Insert: {
+          created_at?: string
+          id?: never
+          metadata?: Json | null
+          name: string
+          source?: string | null
+        }
+        Update: {
+          created_at?: string
+          id?: never
+          metadata?: Json | null
+          name?: string
+          source?: string | null
+        }
+        Relationships: []
+      }
+      tags: {
+        Row: {
+          created_at: string
+          group: string | null
+          id: number
+          name: string
+          value: string
+        }
+        Insert: {
+          created_at?: string
+          group?: string | null
+          id?: number
+          name: string
+          value: string
+        }
+        Update: {
+          created_at?: string
+          group?: string | null
+          id?: number
+          name?: string
+          value?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      match_documents:
+        | {
+            Args: {
+              query_embedding: string
+              match_count?: number
+              filter?: Json
+            }
+            Returns: {
+              id: number
+              content: string
+              metadata: Json
+              similarity: number
+            }[]
+          }
+        | {
+            Args: {
+              query_embedding: string
+              match_threshold: number
+              match_count: number
+            }
+            Returns: {
+              id: number
+              content: string
+              similarity: number
+            }[]
+          }
+      match_documents_new: {
+        Args: {
+          query_embedding: string
+          match_threshold?: number
+          match_count?: number
+          filter?: Json
+        }
+        Returns: {
+          id: number
+          content: string
+          metadata: Json
+          similarity: number
+        }[]
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+type PublicSchema = Database[Extract<keyof Database, 'public'>]
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema['Tables'] & PublicSchema['Views'])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+        Database[PublicTableNameOrOptions['schema']]['Views'])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema['Tables'] &
+        PublicSchema['Views'])
+    ? (PublicSchema['Tables'] &
+        PublicSchema['Views'])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema['Tables']
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+    ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema['Tables']
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+    ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema['Enums']
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema['Enums']
+    ? PublicSchema['Enums'][PublicEnumNameOrOptions]
+    : never
